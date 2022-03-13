66f46e78f577ab024b6f7e36e3d74f5a97f01bbe
edubew <79300226+edubew@users.noreply.github.com>
2022-03-14 01:31:21 +0300

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,6 @@ const body = document.querySelector('body');
 hamburger.addEventListener('click', () => {
   hamburger.classList.toggle('show');
   navbarlist.classList.toggle('show');
-  //body.style.overflow = 'hidden';
   if (body.style.width < 574) {
     logo.innerHTML = '';
   }
